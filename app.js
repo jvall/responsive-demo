250155e1d901793bb27d71aedee8154cef4a83e0
John Vall <jvall@westmonroepartners.com>
2017-05-31 13:11:36 -0500

    basic responsive demo complete

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 $(function() {
   $(".workflow-item").click(function() {
-    $(".workflow-item-info").addClass("open");
-    $(".workflow-list").addClass("open");
+    $(".workflow-item-info").addClass("slide-open");
+    $(".workflow-list").addClass("slide-open");
   });
 
   $(".go-back").click(function() {
-    $(".workflow-item-info").removeClass("open");
-    $(".workflow-list").removeClass("open");
+    $(".workflow-item-info").removeClass("slide-open");
+    $(".workflow-list").removeClass("slide-open");
   });
 });
